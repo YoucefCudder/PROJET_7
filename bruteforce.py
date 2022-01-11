d665bf1c617c00f4765f152036c180803a456610
Youcef Aouali <you.aouali@gmail.com>
2022-01-11 16:43:18 +0100

    travail la fonction bruteforce 11/01 solution incomplète du bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -12,54 +12,37 @@ def read_shares():
     with open('test_shares.csv') as csv_file:
         shares_file = csv.reader(csv_file, delimiter=',')
         shares_list = []
-        MAX_INVEST = 500
         for share_row in shares_file:
             shares_list.append((share_row[0], float(share_row[1]), float(share_row[2])))
-        selected_elements = []
+        shares_selected = []
 
-    for i in range(len(shares_list)):
-        possibilities = combinations(shares_list, i+1)
-        for elements in possibilities:
-            for actions in elements:
-                costs = []
-                gains = []
+        for i in range(len(shares_list)):
+            possibilities = combinations(shares_list, i+1)
+            for elements in possibilities:
                 all_costs = 0
                 all_gains = 0
-                costs.append(actions[1])
-                gains.append(actions[2])
-                for cost in costs:
-                    all_costs += cost
-                for gain in gains:
-                    all_gains += gain
+                costs = []
+                gains = []
+                MAX_INVEST = 500
 
-                if all_costs <= MAX_INVEST:
+                for actions in elements:
+                    costs.append(actions[1])
+                    gains.append(actions[2])
 
-                    selected_elements.append((round(all_gains, 2), actions))
+                    for cost in costs:
+                        all_costs += cost
+                    for gain in gains:
+                        all_gains += gain
 
-        selected_elements = sorted(selected_elements, key=lambda x: -x[0])
+                    if all_costs <= MAX_INVEST:
 
+                        shares_selected.append((round(all_gains, 2), elements))# actions ou elements????????
 
-    print(selected_elements[0])
+        shares_selected = sorted(shares_selected, key=lambda x: -x[0])
 
+    # print(len(shares_selected))
+    print(shares_selected[-0])
 
-def calc_brute(shares_list, MAX_INVEST=500, bag=None):
-
-        if bag is None:
-            bag = []
-        if shares_list:
-            shares_list[0][1], shares_list[0][2] = calc_brute(shares_list[-1:], MAX_INVEST, bag)
-            val = shares_list[0][1]
-            if val <= MAX_INVEST:
-                shares_list[1][1], shares_list[1][2] = calc_brute(MAX_INVEST - shares_list[0][1], shares_list[:1], bag + [val])
-                if shares_list[0][1] < shares_list[1][1]:
-                    return shares_list[1][1], shares_list[1][2]
-
-            return shares_list[0][1], shares_list[0][2]
-
-        else:
-            return sum([i[2] for i in bag]), bag
-
-# print(f'{info[0]}, {info[1]}, {info[2]}')
 
 
 """
@@ -84,4 +67,3 @@ C denotes the cost of the item.
 
 if __name__ == '__main__':
     read_shares()
-
