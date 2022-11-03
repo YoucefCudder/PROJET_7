16b728a69a126c7db4a5239576e913dac75487de
Youcef Aouali <you.aouali@gmail.com>
2022-11-03 01:05:11 +0100

    travail à poursuivre, bonne voie sur optimized

--- bruteforce.py
+++ bruteforce.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # coding: utf8
-# enumerer toutes les combinaisons possibles
-# trier les combinaisons possibles par rapport au maximum
-# choisir la  meilleure combinaison
+
+
+""" enumerer toutes les combinaisons possibles
+ trier les combinaisons possibles par rapport au maximum
+ choisir la  meilleure combinaison"""
 
 import csv
 from itertools import combinations
@@ -11,12 +13,15 @@ MAX_INVEST = 500
 
 
 def read_shares():
+    """
+
+    :return:
+    """
     with open("test_shares.csv") as csv_file:
         shares_file = csv.reader(csv_file, delimiter=",")
         shares_list = []
         for share_row in shares_file:
             shares_list.append((share_row[0], float(share_row[1]), float(share_row[2])))
-        shares_selected = []
         save = []
     for i in range(len(shares_list)):
         possibilities = combinations(shares_list, i + 1)
@@ -32,70 +37,22 @@ def read_shares():
                 cost.append(actions[1])
                 gains.append(benefit)
                 if cost_of_combinaison <= float(MAX_INVEST):
-                    save.append({"combi": elements, "price": cost_of_combinaison, "gain": gain_of_combinaison})
+                    save.append(
+                        {
+                            "combi": elements,
+                            "price": cost_of_combinaison,
+                            "gain": gain_of_combinaison,
+                        }
+                    )
 
     shares_selected = sorted(save, key=lambda k: (k["gain"]), reverse=True)
-    print(shares_selected[0])
+    print(f"Pour une limite de {MAX_INVEST}, il est préférable d' acheter : \n")
 
-    # if sum(cost) < 500 and max(gains) :
-    #    print(sum(cost), max(gains))
+    print(
+        f'{shares_selected[0]["combi"]}, pour un coût total de {round(shares_selected[0]["price"], 2)}€'
+        f'et un gain de {round(shares_selected[0]["gain"], 2)}€'
+    )
 
-    # if sum(cost) < MAX_INVEST:
-    # save.append(cost)
-
-    # print(sum(cost))
-    # sum(range(int(elements[i][1]))
-    # gains_of_combinaison = sum(range(int(elements[i][2])))
-
-
-# cost_of_combinaison += j[1]
-
-
-"""all_costs = 0
-                all_gains = 0
-                costs = []
-                gains = []
-                MAX_INVEST = 500
-
-                for actions in elements:
-                    costs.append(actions[1])
-                    gains.append(actions[2])
-
-                    for cost in costs:
-                        all_costs += cost
-                    for gain in gains:
-                        all_gains += gain
-
-                    if all_costs <= MAX_INVEST:
-
-                        shares_selected.append(
-                            (round(all_gains), (round(all_costs)), elements)
-                        )  # actions ou elements????????
-
-    shares_selected = sorted(shares_selected, key=lambda x: -x[0])
-    # print(all_costs, all_gains)
-    print(len(shares_selected))
-    print(shares_selected[0])
-
-"""
-"""
-N denotes the number of items.
-W denotes the weight of the item.
-C denotes the cost of the item.
-    if elements_selection is None:
-        elements_selection = []
-        if elements:
-            val1, lst_val1 = read_shares(capacite, elements[1:], elements_selection)
-            val = elements[0]
-            if val[1] <= capacite:
-                val2, lst_val2 = read_shares(capacite - val[1], elements[1:], elements_selection + [val])
-                if val1 < val2:
-                    return val2, lst_val2
-
-                return val1, lst_val1
-            else:
-                return sum([i[2] for i in elements_selection]), elements_selection
-"""
 
 if __name__ == "__main__":
     read_shares()
---
+++ test2.py
@@ -0,0 +1,69 @@
+import csv
+
+
+def read_shares(csv_doc):
+    """ """
+    with open(csv_doc) as csv_file:
+        data_list = list(csv.reader(csv_file))
+    selection = []
+    for row in data_list[1:]:
+        if round(float(row[1])) != 0 and float(row[2]) >= 0 and not row[1].startswith("-"):
+            benefit = round(float(row[1]) * float(row[2]) / 100, 2)
+            ratio = round(float(row[2])) / round(float(row[1]))
+
+            selection.append(
+                {
+                    "action": row[0],
+                    "price": row[1],
+                    "value": row[2],
+                    "benefit": benefit,
+                    "ratio": ratio,
+                }
+            )
+
+    return selection
+
+
+def knapsack(value, price):
+    n = len(shares)
+    MAX_INVEST = 500 * 100
+
+    K = [[0 for x in range(MAX_INVEST + 1)] for x in range(n + 1)]
+    # Build table K[][] in bottom up manner
+    for i in range(n + 1):
+        for w in range(MAX_INVEST + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif price[i - 1] <= w:
+                K[i][w] = max(value[i - 1]
+                              + K[i - 1][w - price[i - 1]],
+                              K[i - 1][w])
+            else:
+                K[i][w] = K[i - 1][w]
+
+    return K[n][MAX_INVEST] / 100
+
+
+shares = read_shares("dataset2_P7.csv")
+value = []
+price = []
+for j in shares:
+    value.append(round(float(j['value'])))
+    price.append(round(float(j['price'])))
+knapsack(value, price)
+# n = len(shares)
+# MAX_INVEST = 500
+# benefit = shares['benefit']
+# ratio = shares['price']
+# print(knapsack(MAX_INVEST, benefit=shares('benefit'), ratio, selection))
+
+
+# if n == 0 or MAX_INVEST == 0:
+#     return 0
+# if (price[n - 1] > MAX_INVEST):
+#     return knapsack(MAX_INVEST, value, price, n - 1)
+# else:
+#     return max(
+#         value[n - 1] + knapsack(
+#             MAX_INVEST - price[n - 1], price, value, n - 1),
+#         knapsack(MAX_INVEST, price, value, n - 1))
